f07261d38264fafbc4e91d283ad9b0824efb5f11
Jdavidson <jd9743101@gmail.com>
2023-07-24 10:29:56 +0400

    relation reflexive avis

--- src/avis/entities/avis.entity.ts
+++ src/avis/entities/avis.entity.ts
@@ -1,18 +1,20 @@
 import { Restaurant } from "src/restaurant/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
-import { PrimaryGeneratedColumn, Column, ManyToOne, Entity } from "typeorm";
+import { PrimaryGeneratedColumn, Column, ManyToOne, Entity, JoinTable, ManyToMany } from "typeorm";
 
 @Entity()
 export class Avis {
     @PrimaryGeneratedColumn()
     id: number
     @Column()
-    name: string;
-    @Column()
+    comment: string;
+    @Column({ nullable: true })
     rating: number;
     @ManyToOne(() => User, (user) => user.avis)
     user: User;
     @ManyToOne(() => Restaurant, (restaurant) => restaurant.avis)
     restaurant: Restaurant;
-
+    @ManyToMany(() => Avis, (avis) => avis.reponse, { onDelete: "CASCADE" })
+    @JoinTable()
+    reponse: Avis[];
 }
